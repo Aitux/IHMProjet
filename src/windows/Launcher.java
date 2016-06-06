46d112d7738ee061df6f44b17e612b697bbbf733
allandel <aramis8@hotmail.fr>
2016-06-06 21:19:03 +0200

    croix sexy

--- src/windows/Launcher.java
+++ src/windows/Launcher.java
@@ -5,7 +5,6 @@ import java.awt.EventQueue;
 public class Launcher {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
